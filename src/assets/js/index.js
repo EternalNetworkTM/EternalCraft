47d817775426e4601a63e894db5c867f16d2fd07
Alexander <alexander@alexitx.com>
2023-06-25 22:31:24 +0300

    Header parallax effect

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,9 +1,40 @@
 'use strict';
 
-const ruleEntries = document.querySelector('.rules-container').children;
+const HEADER_IMAGE_OPACITY = 0.4;
+const HEADER_IMAGE_TRANSLATE_PERCENT = 50;
+const HEADER_IMAGE_SCALE_FACTOR = 0.2;
+
+function setupHeaderImageAnimation() {
+    const header = document.querySelector('header');
+    const headerImage = document.querySelector('.background-image');
+
+    function scrollHandler() {
+        const headerRect = header.getBoundingClientRect();
+        const percentVisible = Math.max((headerRect.height + headerRect.top) / headerRect.height, 0);
+        headerImage.style.opacity = HEADER_IMAGE_OPACITY * percentVisible;
+        const translateY = (1 - percentVisible) * HEADER_IMAGE_TRANSLATE_PERCENT;
+        const scale = 1 + (1 - percentVisible) * HEADER_IMAGE_SCALE_FACTOR;
+        headerImage.style.transform = `translateY(${translateY}%) scale(${scale})`;
+    }
+
+    const observer = new IntersectionObserver((entries) => {
+        const headerImageEntry = entries[0];
+        if (headerImageEntry && headerImageEntry.isIntersecting) {
+            document.body.addEventListener('scroll', scrollHandler);
+        } else {
+            document.body.removeEventListener('scroll', scrollHandler);
+        }
+    });
+    observer.observe(header);
+
+    scrollHandler();
+}
+
 const ruleObjects = [];
 
 function setupRules() {
+    const ruleEntries = document.querySelector('.rules-container').children;
+
     for (let i = 0; i < ruleEntries.length; i++) {
         const rule = ruleEntries[i];
         const body = rule.querySelector('.body');
@@ -53,7 +84,7 @@ function collapseRule(ruleObject) {
     }
 }
 
-
 window.addEventListener('load', () => {
+    setupHeaderImageAnimation();
     setupRules();
 });
